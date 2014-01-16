4fa99deb8aceb300bdddd26489fcf3430965c86f
jay <jay@oakley.(none)>
2014-01-16 19:37:45 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ga::Application.config.secret_key_base = '046aaa4cfc825c71132dab6240585d135fa9486598d22fe6b4e9af0ab79852befe60987008083c743cb62dbe9acacb530a1e5bc0064b0778235de2792da9134f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ga::Application.config.session_store :cookie_store, key: '_ga_session'
