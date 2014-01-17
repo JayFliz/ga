c826ad49e1b01d3863e2a922b3b83cc4d9723115
Jay Greasley <jay@fliz.co.uk>
2014-01-17 01:18:08 +0000

    setup pg on laptop

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140116193845) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "animals", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
